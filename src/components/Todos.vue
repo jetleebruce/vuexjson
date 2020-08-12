b5d9a9f623ed468364a07ab6e4c682370ffabcf5
Serge Modin <modinsv@yandex.ru>
2020-08-12 23:31:20 +0300

    Made Todos component and implemented it in Home views. Styled App.vue - added body tag and cover all appearing views with the .container

---
+++ src/components/Todos.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h3>Todos</h3>
+  </div>
+</template>
+<style scoped>
+h3 {
+  color: aliceblue;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <Todos />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Todos from "@/components/Todos.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Todos
   }
 };
 </script>
+
+<style scoped>
+</style>
