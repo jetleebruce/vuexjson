cb845e432cac1cc26d2995a935e71dceef9c9262
Serge Modin <modinsv@yandex.ru>
2020-08-13 13:50:59 +0300

    Delete todo, Filter todo

---
+++ src/components/FilterTodos.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <span>Filter Todos:</span>
+    <select @change="filterTodos($event)">
+      <option value="200">200</option>
+      <option value="100">100</option>
+      <option value="50">50</option>
+      <option value="20">20</option>
+      <option value="10">10</option>
+    </select>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: "FilterTodos",
+  methods: mapActions(["filterTodos"])
+};
+</script>
+
+<style scoped>
+span {
+  margin: 3rem;
+  color: wheat;
+  margin-right: 2rem;
+}
+input {
+  margin-top: 20px;
+  padding: 6px;
+  border: 1px solid #41b883;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <AddTodo/>
+    <AddTodo />
+    <FilterTodos />
     <Todos />
   </div>
 </template>
@@ -8,13 +9,15 @@
 <script>
 // @ is an alias to /src
 import Todos from "@/components/Todos.vue";
-import AddTodo from "@/components/AddTodo.vue"
+import AddTodo from "@/components/AddTodo.vue";
+import FilterTodos from "@/components/FilterTodos.vue";
 
 export default {
   name: "Home",
   components: {
     Todos,
-    AddTodo
+    AddTodo,
+    FilterTodos
   }
 };
 </script>
